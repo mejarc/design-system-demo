a9fe77b1a2b8dc60450e98edc26645d0c9505a55
Melanie Archer <melanie.archer@veeva.com>
2016-04-19 14:09:19 -0700

    Replace LESS with Sass

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sass = require('node-sass');
+var sassMiddleware = require('node-sass-middleware');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sass.middleware({ src: path.join(__dirname, 'public')}));
+app.use(sassMiddleware({ src: path.join(__dirname, 'public')}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
