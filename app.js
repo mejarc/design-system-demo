ad4da163550372f1e238f49773f33785ac003291
Melanie Archer <melanie.archer@veeva.com>
2016-04-19 14:43:03 -0700

    Fix fonts path

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({ src: path.join(__dirname, 'public')}));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/uswds/dist/fonts')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
